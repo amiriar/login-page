b9d06725fa794945ed815f6b4cf6e33750f87fba
amiriar <amiriar.irr@gmail.com>
2023-09-05 14:51:08 +0330

    final commit2

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,21 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { getSession,useSession } from 'next-auth/react';
+import React from 'react';
+import { getSession } from 'next-auth/react';
 
 const dashboard = () => {
-    const [name,setName] = useState("");
-    const [lastname,setLastname] = useState("");
-    const [password,setPassword] = useState("");
-
-    const submitHnadler = async ()=>{
-        const res = await fetch("/api/updateInfo", {
-            method: "POST",
-            body: JSON.stringify({name,lastname,password}),
-            headers: {"Content-Type" : "application/json"}
-        })
-        const userData =  await res.json();
-        console.log(userData);
-    }
-    
 
     return (
         <div style={{width: "100%", display:"flex", height: "100vh", justifyContent: "center", alignItems: "center"}}>
